4d21753b1dcc523b66fc4f43f7a81ecbf382ba4c
kaifJ <kaif.jamadar.in@gmail.com>
2022-11-30 13:44:24 +0200

    change server messages

--- Server_3/index.js
+++ Server_3/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/bookings', bookingRoutes)
 app.use('/', (request, response) => {
-    response.status(200).send('<h1>Hello from server 1</h1>')
+    response.status(200).send('<h1>Hello from server 3</h1>')
 })
 
 
