a3cbd6b709ded7c5d09a1a47687bd9ea88cf2c81
kaifJ <kaif.jamadar.in@gmail.com>
2022-11-30 16:45:40 +0200

    default port

--- Server_2/index.js
+++ Server_2/index.js
@@ -11,7 +11,7 @@ const connectToDB = require('./Connections/mongodb')
 connectToDB()
 connectToCache()
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3001
 
 const app = express()
 const server = http.createServer(app)
