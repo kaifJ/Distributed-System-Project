5e2ad6cfbd848fba99d21d8adde5197b7914dfab
Haseeb Usmani <usmani_haseeb@yahoo.com>
2022-12-10 22:37:51 +0200

    Added toast messages for POST call responses

--- booking/src/views/SeatSelection/SeatSelection.jsx
+++ booking/src/views/SeatSelection/SeatSelection.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { AxiosInstance, config } from "../../utils/api";
 import { GET_SEATS, BOOK_SEATS } from "../../utils/endpoints";
 import Loader from "./Loader";
-import Footer from './Footer'
+import Footer from "./Footer";
 import {
   Button,
   Seats,
@@ -12,30 +12,32 @@ import {
 } from "./SeatSelection.style";
 import { MdEventSeat } from "react-icons/md";
 import { BiArrowBack } from "react-icons/bi";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SeatSelection = (props) => {
   const [seats, setSeats] = useState([]);
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [booked, setBooked] = useState(false)
+  const [booked, setBooked] = useState(false);
 
   useEffect(() => {
     // Get Seats
-    fetch()
+    fetch();
   }, []);
 
   const fetch = () => {
-    AxiosInstance.get(GET_SEATS.replace("{id}", props.screenId)).then((res) => {
+    AxiosInstance.get(GET_SEATS.replace("{id}", props.screenId))
+      .then((res) => {
         setSeats(res.data.seats);
-      }
-    ).catch(err => {
-      console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
 
-      //Network Error Code -> "ERR_NETWORK"
-      // If server rejects a request -> err.response.status should be checked
-
-    });
-  }
+        //Network Error Code -> "ERR_NETWORK"
+        // If server rejects a request -> err.response.status should be checked
+      });
+  };
 
   const selectSeat = (seat) => {
     const newSeats = [...selectedSeats];
@@ -46,48 +48,56 @@ const SeatSelection = (props) => {
   };
 
   const confirmBooking = () => {
-    setLoading(true)
+    setLoading(true);
 
-    let _selectedSeats = selectedSeats.map(seat => seat.id)
-    let body = JSON.stringify({ selectedSeats: _selectedSeats })
+    let _selectedSeats = selectedSeats.map((seat) => seat.id);
+    let body = JSON.stringify({ selectedSeats: _selectedSeats });
 
-    AxiosInstance.post(BOOK_SEATS, body, config).then(result => {
-      fetch()
+    AxiosInstance.post(BOOK_SEATS, body, config)
+      .then((result) => {
+        fetch();
 
-      setSelectedSeats([])
-      setBooked(true)
-      setLoading(false)
-
-    }).catch(err => {
-      console.log(err)
-      setLoading(false)
-      fetch()
-    })
+        setSelectedSeats([]);
+        setBooked(true);
+        setLoading(false);
+        toast.success(result.data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        fetch();
+        toast.error(err.response.data.message);
+      });
   };
 
   return loading ? (
     <Loader loading={loading} booked={booked} />
   ) : (
     <Main>
-      <div style={{
-       display: "flex",
-       alignSelf: "flex-start",
-       justifyContent: "space-around",
-       alignItems: "center",
-       cursor: "pointer",
-       width: "inherit",
-      }}>
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "flex-start",
+          justifyContent: "space-around",
+          alignItems: "center",
+          cursor: "pointer",
+          width: "inherit",
+        }}
+      >
         <BackButton onClick={() => props.goBack()}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
             <BiArrowBack className="back-btn" />
             <span>Back</span>
           </div>
         </BackButton>
-        <Button disabled={!selectedSeats.length} onClick={() => confirmBooking()}>
+        <Button
+          disabled={!selectedSeats.length}
+          onClick={() => confirmBooking()}
+        >
           Confirm Booking
         </Button>
       </div>
-      <div style={{ textAlign: 'center' }}>
+      <div style={{ textAlign: "center" }}>
         <h5>All Eyes this way please </h5>
         <h2>------------Screen------------</h2>
       </div>
@@ -104,7 +114,8 @@ const SeatSelection = (props) => {
           </Seats>
         ))}
       </SeatsContainer>
-      <Footer/>
+      <Footer />
+      <ToastContainer draggable={false} hideProgressBar theme="colored" />
     </Main>
   );
 };
